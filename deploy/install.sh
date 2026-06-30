cf87af61002b10fccef8cb86dfcd1b754f4193be
hmtstc-creator <hmtstc@gmail.com>
2026-06-30 16:03:16 +0300

    version2

--- deploy/install.sh
+++ deploy/install.sh
@@ -25,3 +25,35 @@ source /opt/megatrader/venv/bin/activate
 pip install --upgrade pip
 
 pip install -r /var/www/MegaTrader/deploy/requirements.txt
+
+cp /var/www/MegaTrader/deploy/systemd/megatrader-deploy.service \
+/etc/systemd/system/
+
+cp /var/www/MegaTrader/deploy/systemd/megatrader-webhook.service \
+/etc/systemd/system/
+
+systemctl daemon-reload
+
+
+cp /var/www/MegaTrader/deploy/nginx/megatrader.conf \
+/etc/nginx/sites-available/megatrader
+
+if [ ! -L /etc/nginx/sites-enabled/megatrader ]; then
+    ln -s /etc/nginx/sites-available/megatrader \
+    /etc/nginx/sites-enabled/megatrader
+fi
+
+nginx -t
+systemctl reload nginx
+
+systemctl enable megatrader-webhook.service
+
+systemctl restart megatrader-webhook.service
+
+mkdir -p /var/log/megatrader
+
+touch /var/log/megatrader/webhook.log
+touch /var/log/megatrader/deploy.log
+
+chmod 664 /var/log/megatrader/webhook.log
+chmod 664 /var/log/megatrader/deploy.log
