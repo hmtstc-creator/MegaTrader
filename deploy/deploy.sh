407d44c07686a68fcca127c9c1d7df68589929de
hmtstc-creator <hmtstc@gmail.com>
2026-06-30 18:36:05 +0300

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,14 @@
 
 set -Eeuo pipefail
 
+PROJECT_DIR="/var/www/MegaTrader"
+
 echo "MegaTrader deploy starting..."
+
+cd "$PROJECT_DIR"
+
+git fetch origin
+
+git reset --hard origin/main
+
+echo "MegaTrader deploy completed"
